7ceba76397a28c6481285bcd267f6d3b5958c924
Mitch Denny <mitch@mitchdenny.com>
2013-01-04 11:53:21 +1100

    Fixed invalid URL.
    I accidentally missed the host parameter name in the URL.

--- Zeup/Program.cs
+++ Zeup/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -16,12 +17,19 @@ namespace Zeup
 
             var request = CreateRequest(hostname, username, password);
             var response = request.GetResponse();
+            var stream = response.GetResponseStream();
+
+            using (var reader = new StreamReader(stream))
+            {
+                var output = reader.ReadToEnd();
+                Console.WriteLine(output);
+            }
         }
 
         private static HttpWebRequest CreateRequest(string hostname, string username, string password)
         {
             var credentials = new NetworkCredential(username, password);
-            var url = string.Format("http://dynamic.zoneedit.com/auth/dynamic.html?{0}", hostname);
+            var url = string.Format("http://dynamic.zoneedit.com/auth/dynamic.html?host={0}", hostname);
             var request = (HttpWebRequest)HttpWebRequest.Create(url);
             request.Credentials = credentials;
             return request;
